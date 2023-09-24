01c795d26364a769f563b38805015efdcacc3a64
jessa602 <jessa602@gmail.com>
2023-09-24 03:39:52 -0700

    start of side bar and title update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
-import Products from "./products/Products";
+import { Routes, Route } from "react-router-dom";
+import AllProducts from "./products/Products";
+import DetailCard from "./products/DetailCard";
+import Nav from "./Nav/Nav";
+import { GiGems } from "react-icons/gi";
 
 function App() {
   return (
     <>
-      <Products />
+      <Nav />
+      <div className="title-container">
+        <h1 className="site-title">
+          Jess's Gems <GiGems />{" "}
+        </h1>
+      </div>
+      <Routes>
+        <Route path="/" element={<AllProducts />} />
+        <Route path="/DetailCard" element={<DetailCard />} />
+      </Routes>
     </>
   );
 }
---
+++ src/products/ExpandedCard.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "../App.css";
+import AllProducts from "./Products";
+import "./expandedCard.css";
+
+const ReadMore = ({ children }) => {
+  const text = children;
+  const [isReadMore, setIsReadMore] = useState(true);
+  const toggleReadMore = () => {
+    setIsReadMore(!isReadMore);
+  };
+  return (
+    <p className="text">
+      {isReadMore ? text.slice(0, 150) : text}
+      <span onClick={toggleReadMore} className="read-or-hide">
+        {isReadMore ? "...read more" : " show less"}
+      </span>
+    </p>
+  );
+};
+
+const Content = ({ description }) => {
+  return (
+    <div className="container2">
+      <p>
+        <ReadMore>{description}</ReadMore>
+      </p>
+    </div>
+  );
+};
+
+export default Content;
